bedbee8044776870f7adb9666278f3708e485657
ChingChingHuang <huang27c@mtholyoke.edu>
2018-08-05 10:10:38 +0800

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by SUN on 8/5/18.
+//  Copyright © 2018 iOS Game. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
